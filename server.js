dd822bee969af137a7b99e5f4940acdd6feec46b
Sergey <box10@stim.by>
2019-01-03 13:15:29 +0300

    Изменил порт

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8085});
+const wss = new WebSocket.Server({ port: 8080});
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
