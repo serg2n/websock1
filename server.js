5c2b78e6496fef24a4dd9e77a4789015164dc265
Sergey <box10@stim.by>
2019-01-03 14:06:36 +0300

    port 8085

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 8085 });
 
 // Broadcast to all.
 wss.broadcast = function broadcast(data) {
